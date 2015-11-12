301b4216f4948fe2a7a14c992a375dbc7355fca9
Joao Vasques <joao.vasques@tecnico.ulisboa.pt>
2015-11-12 19:14:14 +0000

    refactors due to persistence message protocol changes

--- src/main/scala/io/joaovasques/modules/core/CoreActor.scala
+++ src/main/scala/io/joaovasques/modules/core/CoreActor.scala
@@ -10,6 +10,8 @@ import play.module.io.joaovasques.playspark.stats.StatsMessages._
 import play.module.io.joaovasques.playspark.execution.{JobExecutionActor}
 import play.module.io.joaovasques.playspark.persistence.{PersistenceActor}
 import play.module.io.joaovasques.playspark.stats.StatsActor
+import play.module.io.joaovasques.playspark.spark.SparkActor
+import play.module.io.joaovasques.playspark.spark.SparkMessages._
 
 object CoreActor extends NamedActor {
   override final val name = "CoreActor"
@@ -18,27 +20,36 @@ object CoreActor extends NamedActor {
 class CoreActor @Inject()(
   @Named(JobExecutionActor.name) jobExecutionActor: ActorRef,
   @Named(PersistenceActor.name) persistenceActor: ActorRef,
-  @Named(StatsActor.name) statsActor: ActorRef
+  @Named(StatsActor.name) statsActor: ActorRef,
+  @Named(SparkActor.name) sparkActor: ActorRef
 ) extends Actor {
 
   private def handleJobExecutionRequest: Receive = {
-    case m @ StartJob(_) => jobExecutionActor ! m
+    case m @ StartJob(_) => {
+      jobExecutionActor ! m
+      sender ! new JobStarted()
+    }
   }
 
   private def handlePersistenceRequest: Receive = {
-    case m @  PersistJob(_, _) => persistenceActor ! m
-    case m @ (GetJob(_) | GetJobs(_, _)) => persistenceActor forward m
+    case m @ (Insert(_,_) | Update(_,_,_,_,_) | Delete(_,_,_)) => persistenceActor ! m
+    case m @ Find(_,_,_,_) => persistenceActor forward m
   }
 
   private def handleStatsRequest: Receive = {
     case m @ (GetJobsInExecution(_) | GetCompletedJobs(_) | GetFailedJobs(_)) => statsActor forward m
   }
 
+  private def handleSparkRequest: Receive = {
+    case m @ GetContext(_) => sparkActor forward m
+    case m @ SaveContext(_) => sparkActor ! m
+  }
+
   private def unhandled: Receive = {
     case m @ _ => sender ! akka.actor.Status.Failure(new MatchError(m))
   }
 
-  def receive = handleJobExecutionRequest orElse handlePersistenceRequest orElse handleStatsRequest orElse unhandled
+  def receive = handleJobExecutionRequest orElse handlePersistenceRequest orElse handleStatsRequest orElse handleSparkRequest orElse  unhandled
   
 }
 
