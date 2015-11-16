c0c726873941398a3fd212e611bc66c3052f8027
Joao Vasques <joao.vasques@tecnico.ulisboa.pt>
2015-11-16 23:30:32 +0000

    adds test on fetch spark context

--- src/test/scala/SparkSpec.scala
+++ src/test/scala/SparkSpec.scala
@@ -7,6 +7,7 @@ import com.google.inject._
 import com.google.inject.name.Named
 import config.ConfigModule
 import play.module.io.joaovasques.playspark.akkaguice.AkkaModule
+import play.module.io.joaovasques.playspark.spark.SparkMessages.GetContext
 import play.module.io.joaovasques.playspark.spark.SparkMessages.SaveContext
 import play.module.io.joaovasques.playspark.spark.SparkModule
 import net.codingwell.scalaguice.ScalaModule
@@ -26,24 +27,34 @@ import scala.concurrent.duration._
 import play.module.io.joaovasques.playspark.tests.TestTraits._
 import org.apache.spark.{SparkContext, SparkConf}
 import scala.concurrent.ExecutionContext.Implicits.global
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 class SparkSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender with WordSpecLike
     with Matchers with BeforeAndAfterAll {
 
-  def this() = this(ActorSystem("SparkSpec"))
+  private final implicit val duration = 5.seconds
+  private final implicit val timeout = Timeout(5 seconds)
 
+  def this() = this(ActorSystem("SparkSpec"))
+      
   override def afterAll(): Unit = {
     TestKit.shutdownActorSystem(system)
   }
-
+      
+  private var sparkAppid: String = _
+      
   "A Spark Actor must" must {
     "submit a unique Spark correctly" in new Core {
       override def _sys = _system
-      implicit val duration = 5.seconds
+      
       val conf = new SparkConf()
         .setMaster("local[2]")
         .setAppName("SparkSpec")
       val sc = new SparkContext(conf)
+      sparkAppid = sc.applicationId
       core ! new SaveContext(sc)
 
 
@@ -52,5 +63,21 @@ class SparkSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSend
       sc.stop()
       result === 1
     }
+
+    "fetch an existing Spark context" in new Core {
+      override def _sys = _system
+
+      val futureResult = (core ? new GetContext(sparkAppid)).mapTo[Option[SparkContext]]
+      Await.result(futureResult, duration) match {
+        case Some(sc) => {
+          val result = sc.applicationId
+          sc.stop()
+          result === sparkAppid
+
+        }
+        case None => fail(s"Not Spark Context was found for app id ${sparkAppid}")
+      }
+    }
   }
 }
+
