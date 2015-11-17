faff6fd3e0e2bb197ff01e811fddccf723ed3e49
Joao Vasques <joao.vasques@tecnico.ulisboa.pt>
2015-11-17 22:13:56 +0000

    change spark context message protocol

--- src/main/scala/io/joaovasques/modules/spark/SparkMessages.scala
+++ src/main/scala/io/joaovasques/modules/spark/SparkMessages.scala
@@ -1,15 +1,19 @@
 package play.module.io.joaovasques.playspark.spark
 
-import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import play.module.io.joaovasques.playspark.api.SparkJob
 
 object SparkMessages {
 
-  case class GetContext(contextId: String)
-  case class GetContexts()
-  case class SaveContext(context: SparkContext)
+  trait SparkContextMessage
 
-  case class StopContext(contextId: String)
-  case class RestartContext(contextId: String)
-  case class DeleteContext(contextId: String)
+  case class GetContexts() extends SparkContextMessage
+  case class SaveContext(conf: SparkConf) extends SparkContextMessage
+  case class StartContext(contextId: String) extends SparkContextMessage
+  case class StopContext() extends SparkContextMessage
+  case class DeleteContext(contextId: String) extends SparkContextMessage
+
+  trait SparkJobMessage
+  case class StartSparkJob(job: SparkJob, contextId: String) extends SparkJobMessage
 }
 
