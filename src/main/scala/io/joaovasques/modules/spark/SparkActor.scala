e1e0c7ff58d41ba3793bfb52c85c3bd4ad05c345
Joao Vasques <joao.vasques@tecnico.ulisboa.pt>
2015-11-17 22:16:07 +0000

    spark actor now handles all things spark context related. Spins out job workers to process spark jobs

--- src/main/scala/io/joaovasques/modules/spark/SparkActor.scala
+++ src/main/scala/io/joaovasques/modules/spark/SparkActor.scala
@@ -2,8 +2,12 @@ package play.module.io.joaovasques.playspark.spark
 
 import akka.actor.{Actor, ActorRef, Props}
 import akka.pattern.ask
+import akka.routing.ActorRefRoutee
+import akka.routing.Router
+import java.util.UUID
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 import scala.language.postfixOps
 import akka.util.Timeout
@@ -11,7 +15,6 @@ import play.api.libs.json.JsValue
 import play.module.io.joaovasques.playspark.akkaguice.NamedActor
 import com.google.inject.{BindingAnnotation, Inject}
 import com.google.inject.name.Named
-import play.module.io.joaovasques.playspark.execution.JobExecutionMessages._
 import play.module.io.joaovasques.playspark.persistence.PersistenceActor
 import play.module.io.joaovasques.playspark.spark.SparkMessages._
 import play.module.io.joaovasques.playspark.spark.SparkImplicits._
@@ -21,6 +24,9 @@ import scala.util.Failure
 import scala.util.Success
 import reflect.ClassTag
 import scala.util.Try
+import play.module.io.joaovasques.playspark.spark.workers.{SparkJobWorker}
+import java.util.concurrent.Executors._
+
 
 object SparkActor extends NamedActor {
   override final val name = "SparkActor"
@@ -30,6 +36,9 @@ class SparkActor @Inject()(
   @Named(PersistenceActor.name) persistenceActor: ActorRef
 ) extends Actor {
 
+  private var currentSparkContext: SparkContext = null
+  private final val maximumRunningJobs = Runtime.getRuntime.availableProcessors
+  private val executionContext: ExecutionContext = ExecutionContext.fromExecutorService(newFixedThreadPool(maximumRunningJobs))
   private final implicit val timeout = Timeout(5 seconds)
 
   private def getSparkContext[T](query: Find)(onSuccess : T => Unit )(onFailure: Throwable => Unit)(implicit tag: ClassTag[T] ): Unit = {
@@ -46,25 +55,14 @@ class SparkActor @Inject()(
     }
   }
 
-  private def handleGetSparkContext: Receive = {
-    case request @ GetContext(id) => {
-      val _sender = sender
-      val query = new Find("spark_app_id", id, "contexts")
-      getSparkContext[Option[JsValue]](query){result: Option[JsValue] =>
-        _sender ! (result.map(r => new SparkContext(r: SparkConf)))
-      } {failure: Throwable =>
-         _sender ! failure
-      }
-    }
-  }
-
   private def handleGetContexts: Receive = {
-    case request @ GetContexts => {
+    case request @ GetContexts() => {
       val _sender = sender
-      val query = new Find("", "", "contexts")
+      val query = new Find("", "", "contexts", false)
       getSparkContext[List[JsValue]](query) {result =>
         _sender ! result.map(c => c: SparkConf)
       } {failure =>
+        println(failure.getStackTraceString)
         _sender ! failure
       }
     }
@@ -73,42 +71,36 @@ class SparkActor @Inject()(
   private def handleSaveContext: Receive = {
     case request @ (_: SaveContext) => {
       val _sender = sender
-      val query = new Insert(sparkConfToJson(request.context.getConf), "contexts")
+      if(currentSparkContext == null) {
+        currentSparkContext = new SparkContext(request.conf)
+      }
+
+      val query = new Insert(sparkConfToJson(currentSparkContext.getConf), "contexts")
       val successResponse = new Success((): Unit)
         (persistenceActor ? query).mapTo[Try[Unit]].onComplete(sendTryResponse[Success[Unit]](_, _sender, successResponse))
     }
   }
 
   private def handleStopContext: Receive = {
-    case request @ StopContext(id) => {
-      val _sender = sender
-      val query = new Find("spark_app_id", id, "contexts")
-      getSparkContext[Option[JsValue]](query) {result =>
-        result match {
-          case Some(config) => {
-            val sparkContext = new SparkContext(config: SparkConf)
-            sparkContext.stop()
-            _sender ! new Success((): Unit)
-          }
-          case None => {
-            _sender ! new Failure(new Exception("TODO"))
-            //TODO log that context was not found
-          }
-        }
-      } {failure =>
-         _sender ! failure
-        //TODO log that context was not found
+    case request @ StopContext() => {
+      if(currentSparkContext != null) {
+        currentSparkContext.stop()
+        currentSparkContext = null
+        sender ! new Success((): Unit)
+      } else {
+        sender ! new Failure(new Exception("No running context"))
       }
     }
   }
 
-  private def handleRestartContext: Receive = {
-    case request @ RestartContext(id) => {
+  private def handleStartContext: Receive = {
+    case request @ StartContext(id) => {
       val _sender = sender
       val query = new Find("spark_app_id", id, "contexts")
       getSparkContext[Option[JsValue]](query) {
         case Some(result) => {
-          _sender ! ( new SparkContext(result: SparkConf))
+          currentSparkContext = new SparkContext(result: SparkConf)
+          _sender ! new Success((): Unit)
         }
         case None => {}
       } {failure =>
@@ -120,12 +112,17 @@ class SparkActor @Inject()(
   //FIND AND STOP CONTEXT
   private def handleDeleteContext: Receive = {
     case request @ DeleteContext(id) => {
+      if(currentSparkContext != null) {
+        if(currentSparkContext.applicationId == id) {
+          currentSparkContext.stop()
+          currentSparkContext = null
+        }
+      }
+
       val _sender = sender
       val query = new Find("spark_app_id", id, "contexts")
       getSparkContext[Option[JsValue]](query) {
-        case Some(result) => {
-          val sparkContext = new SparkContext(result: SparkConf)
-          sparkContext.stop()
+        case Some(_) => {
           val deleteQuery = new Delete("spark_app_id", id, "contexts")
             (persistenceActor ? deleteQuery).mapTo[Try[Unit]].onComplete(
               sendTryResponse[Success[Unit]](_, _sender, new Success((): Unit))
@@ -141,18 +138,19 @@ class SparkActor @Inject()(
     }
   }
 
-  private def unhandled: Receive = {
-    case _ => {
-
+  private def handleJobSubmission: Receive = {
+    case message : SparkJobMessage => {
+      val workerId = UUID.randomUUID().toString()
+      context.actorOf(SparkJobWorker.props(executionContext), name = s"SparkJobWorker-${workerId}") forward message
     }
   }
 
-  def receive = handleGetSparkContext orElse
-  handleGetContexts orElse
-  handleSaveContext orElse
-  handleStopContext orElse
-  handleRestartContext orElse
-  handleDeleteContext orElse
-  unhandled
+  def receive = handleGetContexts orElse
+    handleSaveContext orElse
+    handleStopContext orElse
+    handleStartContext orElse
+    handleDeleteContext orElse
+    handleJobSubmission
+  
 }
 
