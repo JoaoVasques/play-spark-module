b2698d121e3252bb684e452bb50733b9b91fb337
Joao Vasques <joao.vasques@tecnico.ulisboa.pt>
2015-11-23 21:55:01 +0000

    updates build and adds serialisable to trait to fix spark bug

--- src/main/scala/io/joaovasques/jobapi/SparkJob.scala
+++ src/main/scala/io/joaovasques/jobapi/SparkJob.scala
@@ -2,7 +2,7 @@ package play.module.io.joaovasques.playspark.api
 
 import org.apache.spark.SparkContext
 
-trait SparkJob {
+trait SparkJob extends Serializable{
 
   def runJob(context: SparkContext): Any
 }
