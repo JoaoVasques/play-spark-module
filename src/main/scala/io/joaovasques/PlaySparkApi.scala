aa1d7d34de78756ceeedbec896ea4a36e2d91c2a
Joao Vasques <joao.vasques@tecnico.ulisboa.pt>
2015-11-24 17:46:27 +0000

    adds module and api for play framework

---
+++ src/main/scala/io/joaovasques/PlaySparkApi.scala
@@ -0,0 +1,111 @@
+package play.modules.io.joaovasques.playspark
+
+import akka.actor.ActorRef
+import com.google.inject.Injector
+import javax.inject._
+import akka.actor.ActorSystem
+import play.api.Configuration
+import play.api.Logger
+import play.api.inject.ApplicationLifecycle
+import scala.concurrent.Await
+import scala.util.Try
+import org.apache.spark.{SparkConf, SparkContext}
+import scala.concurrent.Future
+import play.modules.io.joaovasques.playspark.api.SparkJob
+import annotation.implicitNotFound
+import akka.pattern.ask
+import akka.util.Timeout
+import com.google.inject.Guice
+import play.modules.io.joaovasques.playspark.config._
+import play.modules.io.joaovasques.playspark.akkaguice.AkkaModule
+import play.modules.io.joaovasques.playspark.akkaguice.GuiceAkkaExtension
+import play.modules.io.joaovasques.playspark.core.CoreActor
+import play.modules.io.joaovasques.playspark.core.CoreModule
+import play.modules.io.joaovasques.playspark.api.SparkJob
+import play.modules.io.joaovasques.playspark.persistence._
+import play.modules.io.joaovasques.playspark.spark.SparkModule
+import play.modules.io.joaovasques.playspark.spark.SparkMessages._
+import play.modules.io.joaovasques.playspark.stats.StatsModule
+import net.codingwell.scalaguice.InjectorExtensions._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+trait PlaySparkApi {
+
+  // Context API
+  def saveSparkContext(conf: SparkConf)(implicit timeout: Timeout): Future[Try[String]]
+  def startContext(contextId: String)(implicit timeout: Timeout): Future[Try[Unit]]
+  def getContextsConfig()(implicit timeout: Timeout): Future[List[SparkConf]]
+  def deleteContext(contextId: String)(implicit timeout: Timeout): Future[Try[Unit]]
+  def stopContext()(implicit timeout: Timeout): Future[Try[Unit]]
+
+  // Job API
+  def startJob(job: SparkJob, contextId: String)(implicit timeout: Timeout): Future[SparkJobResult]
+  def startSyncJob(job: SparkJob, contextId: String)(implicit timeout: Timeout): SparkJobResult
+}
+
+final class PlaySparkApiImplementation @Inject() (
+  actorSystem: ActorSystem,
+  configuration: Configuration,
+  applicationLifecycle: ApplicationLifecycle
+) extends PlaySparkApi {
+
+  private final val injector = Guice.createInjector(
+    new ConfigModule(),
+    new AkkaModule(),
+    new PersistenceModule(),
+    new StatsModule(),
+    new SparkModule(),
+    new CoreModule()
+  )
+
+  private final lazy val system = injector.instance[ActorSystem]
+  private final lazy val coreActor = system.actorOf(GuiceAkkaExtension(system).props(CoreActor.name))
+
+  private def registerDriverShutdown(
+    coreActor: ActorRef,
+    system: ActorSystem
+  ): Unit = applicationLifecycle.addStopHook(() => {
+    Logger.info("PlaySpark stopping..")
+    Future {
+      //TODO KILL OTHER ACTORS SMOOTHLY
+      system.shutdown()
+    }
+  })
+
+  @implicitNotFound(msg = "Cannot find implicit timeout") 
+  def saveSparkContext(conf: SparkConf)(implicit timeout: Timeout): Future[Try[String]] = {
+    (coreActor ? new SaveContext(conf)).mapTo[Try[String]]
+  }
+
+  @implicitNotFound(msg = "Cannot find implicit timeout")
+  def startContext(contextId: String)(implicit timeout: Timeout): Future[Try[Unit]] = {
+    (coreActor ? new StartContext(contextId)).mapTo[Try[Unit]]
+  }
+
+  @implicitNotFound(msg = "Cannot find implicit timeout")
+  def getContextsConfig()(implicit timeout: Timeout): Future[List[SparkConf]] = {
+    (coreActor ? new GetContexts()).mapTo[List[SparkConf]]
+  }
+
+  @implicitNotFound(msg = "Cannot find implicit timeout")
+  def deleteContext(contextId: String)(implicit timeout: Timeout): Future[Try[Unit]] = {
+    (coreActor ? new DeleteContext(contextId)).mapTo[Try[Unit]]
+  }
+
+  @implicitNotFound(msg = "Cannot find implicit timeout")
+  def stopContext()(implicit timeout: Timeout): Future[Try[Unit]] = {
+    (coreActor ? new StopContext()).mapTo[Try[Unit]]
+  }
+
+  @implicitNotFound(msg = "Cannot find implicit timeout")
+  def startJob(job: SparkJob, contextId: String)(implicit timeout: Timeout): Future[SparkJobResult] = {
+    (coreActor ? new StartSparkJob(job, contextId, true)).mapTo[SparkJobResult]
+  }
+
+  @implicitNotFound(msg = "Cannot find implicit timeout duration")
+  def startSyncJob(job: SparkJob, contextId: String)(implicit timeout: Timeout): SparkJobResult = {
+    val futureResult = (coreActor ? new StartSparkJob(job, contextId, true)).mapTo[SparkJobResult]
+    Await.result(futureResult, timeout.duration)
+  }
+}
+
